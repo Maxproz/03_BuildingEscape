8ac4456ba06cfcdc88161d775c81fbcf32f707a6
Maxpro <maxdietz@hotmail.com>
2016-11-09 08:23:41 -0800

    Implemented blueprints for the doors using relative locations.

--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -5,6 +5,7 @@
 #include "Components/ActorComponent.h"
 #include "OpenDoor.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FDoorEvent);
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
@@ -17,24 +18,22 @@ public:
 
 	// Called when the game starts
 	virtual void BeginPlay() override;
-	
+
 	// Called every frame
 	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
     
-    void OpenDoor();
-    void CloseDoor();
+    UPROPERTY(BlueprintAssignable)
+    FDoorEvent OnOpen;
+    
+    UPROPERTY(BlueprintAssignable)
+    FDoorEvent OnClose;
     
 private:
-    UPROPERTY(EditAnywhere)
-    float OpenAngle = -90.0f;
-    
     UPROPERTY(EditAnywhere)
     ATriggerVolume* PressurePlate = nullptr;
     
     UPROPERTY(EditAnywhere)
-    float DoorCloseDelay = 1.0f;
-    
-    float LastDoorOpenTime;
+    float TriggerMass = 30.0f;
     
     // The owning door
     AActor* Owner = nullptr;
